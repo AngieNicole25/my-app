cc64eaf01cc17b841e5914e8587ef323dcde0151
Angie Nicole <angieandrade9601@gmail.com>
2023-04-23 15:01:25 -0500

    desafio3

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -15,6 +15,7 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
+import stylesNavbar from '../styles/Navbar.module.css'
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -156,7 +157,7 @@ export default function PrimarySearchAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar className={stylesNavbar.Navbar} position="static">
         <Toolbar>
           <IconButton
             size="large"
@@ -187,18 +188,14 @@ export default function PrimarySearchAppBar() {
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
             <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="error">
                 <MailIcon />
-              </Badge>
             </IconButton>
             <IconButton
               size="large"
               aria-label="show 17 new notifications"
               color="inherit"
             >
-              <Badge badgeContent={17} color="error">
                 <NotificationsIcon />
-              </Badge>
             </IconButton>
             <IconButton
               size="large"
